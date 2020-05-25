7e2ec4e82471ae2c202cce43fe707db36fdce777
yochirolee <yleecruz@gmail.com>
2020-05-24 22:33:36 -0500

    Updates

---
+++ precache-manifest.7a37536281763a854447994f7bbabb6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a439d028b5993efe1ea200f61d14b0",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "f55a9d20b59bbc5caf69",
+    "url": "/movies/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "364e978f1a9ade1cbc6a",
+    "url": "/movies/static/css/main.94a281fa.chunk.css"
+  },
+  {
+    "revision": "f55a9d20b59bbc5caf69",
+    "url": "/movies/static/js/2.e6b88131.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies/static/js/2.e6b88131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "364e978f1a9ade1cbc6a",
+    "url": "/movies/static/js/main.19cfaeea.chunk.js"
+  },
+  {
+    "revision": "0c2cc06daa5ab8faa790",
+    "url": "/movies/static/js/runtime-main.e21949be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movies/static/media/logo.5d5d9eef.svg"
+  }
+]);
